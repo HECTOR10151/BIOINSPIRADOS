c6f7174e315827918fb29134aee25a9657f78e29
HECTOR101510 <ruizhernandezhector57@gmail.com>
2024-04-08 21:18:41 -0600

    arreglo en binario

--- generar.c
+++ generar.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int binario(int n);
 int main() {
     int tam, min, max, i, j, temp, maxVal;
-    int *arr;
+    int *arr,*bin;
     printf("Ingrese el tamaño del arreglo: ");
     scanf("%d", &tam);
     printf("Ingrese el rango mínimo: ");
@@ -11,6 +12,7 @@ int main() {
     printf("Ingrese el rango máximo: ");
     scanf("%d", &max);
     arr = (int*) malloc(tam * sizeof(int));
+    bin = (int*) malloc(tam * sizeof(int));
     srand(time(0));
     for(i = 0; i < tam; i++) {
         arr[i] = (rand() % (max - min + 1)) + min;
@@ -25,10 +27,25 @@ int main() {
         }
     }
     maxVal = arr[tam-1];
-    printf("Arreglo ordenado: ");
+    printf("Arreglo ordenado y en decimal: ");
     for(i = 0; i < tam; i++) {
-        printf("%d ", arr[i]);
+        printf("[%d] ", arr[i]);
+        bin[i]=binario(arr[i]);
     }
     printf("\nValor máximo: %d\n", maxVal);
+    printf("Arreglo ordenado y en binario: ");
+    for(i = 0; i < tam; i++) {
+        printf("[%d] ", bin[i]);
+    }
+    printf("\nValor máximo: %d\n", bin[tam-1]);
     return 0;
+}
+int binario(int n){
+    int bin = 0, i = 1;
+    while(n > 0){
+        bin += (n % 2) * i;
+        n /= 2;
+        i *= 10;
+    }
+    return bin;
 }
